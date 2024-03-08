db295f841b40c34617bcb2a6ae2692475b555f04
Symm <155481577+SymmGitHub@users.noreply.github.com>
2024-03-07 22:55:29 -0500

    Add files via upload

---
+++ ChainPellet/panels/sf/LightEditor.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ChainPellet.panels.sf
+{
+    partial class LightEditor
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            colorBox = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            rBox = new TextBox();
+            gBox = new TextBox();
+            bBox = new TextBox();
+            SuspendLayout();
+            // 
+            // colorBox
+            // 
+            colorBox.ForeColor = SystemColors.ControlText;
+            colorBox.Location = new Point(40, 20);
+            colorBox.Name = "colorBox";
+            colorBox.Size = new Size(125, 125);
+            colorBox.TabIndex = 0;
+            colorBox.UseVisualStyleBackColor = true;
+            colorBox.Click += colorBox_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(175, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(35, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Red";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(175, 89);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Blue";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(175, 56);
+            label3.Name = "label3";
+            label3.Size = new Size(48, 20);
+            label3.TabIndex = 4;
+            label3.Text = "Green";
+            // 
+            // rBox
+            // 
+            rBox.Location = new Point(229, 20);
+            rBox.Name = "rBox";
+            rBox.Size = new Size(125, 27);
+            rBox.TabIndex = 8;
+            rBox.TextChanged += colorBoxesChanged;
+            // 
+            // gBox
+            // 
+            gBox.Location = new Point(229, 53);
+            gBox.Name = "gBox";
+            gBox.Size = new Size(125, 27);
+            gBox.TabIndex = 9;
+            gBox.TextChanged += colorBoxesChanged;
+            // 
+            // bBox
+            // 
+            bBox.Location = new Point(229, 86);
+            bBox.Name = "bBox";
+            bBox.Size = new Size(125, 27);
+            bBox.TabIndex = 10;
+            bBox.TextChanged += colorBoxesChanged;
+            // 
+            // LightEditor
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(64, 64, 68);
+            Controls.Add(bBox);
+            Controls.Add(gBox);
+            Controls.Add(rBox);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(colorBox);
+            ForeColor = SystemColors.Control;
+            Name = "LightEditor";
+            Size = new Size(395, 300);
+            Load += LightEditor_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button colorBox;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox rBox;
+        private TextBox gBox;
+        private TextBox bBox;
+    }
+}
---
+++ ChainPellet/panels/sf/LightEditor.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static ChainPellet.sf;
+
+namespace ChainPellet.panels.sf
+{
+    public partial class LightEditor : UserControl
+    {
+        public MainForm main;
+        public SceneNode loadedNode;
+        public bool loading;
+        public LightEditor(MainForm mainForm, SceneNode node)
+        {
+            InitializeComponent();
+            loadedNode = node;
+            main = mainForm;
+        }
+        private void LightEditor_Load(object sender, EventArgs e)
+        {
+            if (loading) return;
+            loading = true;
+            Light l = loadedNode.data.light;
+            rBox.Text = l.r.ToString();
+            gBox.Text = l.g.ToString();
+            bBox.Text = l.b.ToString();
+            colorBox.BackColor = ColorFromNodeRGB(l.r, l.g, l.b);
+            loading = false;
+        }
+        public Color ColorFromNodeRGB(float r, float g, float b)
+        {
+            Color col = Color.White;
+            try
+            {
+                if (r > 1) r = 1;
+                else if (r < 0) r = 0;
+
+                if (g > 1) g = 1;
+                else if (g < 0) g = 0;
+
+                if (b > 1) b = 1;
+                else if (b < 0) b = 0;
+
+                int ir = (int)(r * 255f);
+                int ig = (int)(g * 255f);
+                int ib = (int)(b * 255f);
+                col = Color.FromArgb(ir, ig, ib);
+            }
+            catch { }
+            return col;
+        }
+
+        private void colorBox_Click(object sender, EventArgs e)
+        {
+            ColorDialog col = new ColorDialog();
+            if (!String.IsNullOrEmpty(Properties.Settings.Default.CustomColorList))
+            {
+                col.CustomColors = LoadCustomColors();
+            }
+            if (col.ShowDialog() == DialogResult.OK)
+            {
+                SaveCustomColors(col.CustomColors);
+                loading = true;
+                float R = (col.Color.R / 255f);
+                float G = (col.Color.G / 255f);
+                float B = (col.Color.B / 255f);
+                rBox.Text = R.ToString();
+                gBox.Text = G.ToString();
+                bBox.Text = B.ToString();
+                loading = false;
+                colorBoxesChanged(sender, e);
+            }
+        }
+
+        private int[] LoadCustomColors()
+        {
+            List<int> colorList = new List<int>();
+            string[] colors = Properties.Settings.Default.CustomColorList.Split(":", StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < colors.Length; i++)
+            {
+                colorList.Add(int.Parse(colors[i]));
+            }
+            return colorList.ToArray();
+        }
+        private void SaveCustomColors(int[] colors)
+        {
+            string colString = "";
+            for (int i = 0; i < colors.Length; i++)
+            {
+                colString += colors[i].ToString() + ":";
+            }
+            Properties.Settings.Default.CustomColorList = colString;
+            Properties.Settings.Default.Save();
+        }
+
+        private void colorBoxesChanged(object sender, EventArgs e)
+        {
+            if (loading) return;
+            loading = true;
+            try
+            {
+                Light l = loadedNode.data.light;
+                l.r = float.Parse(rBox.Text);
+                l.g = float.Parse(gBox.Text);
+                l.b = float.Parse(bBox.Text);
+                loadedNode.data.light = l;
+                main.OverwriteNode(loadedNode);
+                colorBox.BackColor = ColorFromNodeRGB(l.r, l.g, l.b);
+            }
+            catch
+            {
+                colorBox.BackColor = ColorFromNodeRGB(1, 1, 1);
+            }
+
+            loading = false;
+        }
+    }
+}
