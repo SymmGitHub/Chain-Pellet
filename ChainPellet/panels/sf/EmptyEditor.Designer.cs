2f281203a14daefa87ca9d2a10bf8d99ebfa11eb
Symm <155481577+SymmGitHub@users.noreply.github.com>
2024-03-07 22:36:36 -0500

    Add files via upload

---
+++ ChainPellet/panels/sf/EmptyEditor.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ChainPellet.panels.sf
+{
+    partial class EmptyEditor
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.Control;
+            label1.Location = new Point(23, 110);
+            label1.Name = "label1";
+            label1.Size = new Size(346, 75);
+            label1.TabIndex = 86;
+            label1.Text = "No additional data is available to edit for this type of Node.\r\n\r\n";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // EmptyEditor
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(64, 64, 68);
+            Controls.Add(label1);
+            Name = "EmptyEditor";
+            Size = new Size(395, 300);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
