bb9129fa9112dc1d271114d8108cfd24c45b27d6
Symm <155481577+SymmGitHub@users.noreply.github.com>
2024-03-07 22:23:03 -0500

    Add files via upload

---
+++ ChainPellet/AddNode.Designer.cs
@@ -0,0 +1,412 @@
+﻿namespace ChainPellet
+{
+    partial class AddNode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label12 = new Label();
+            wScale = new TextBox();
+            modelName = new TextBox();
+            label11 = new Label();
+            wRot = new TextBox();
+            geomName = new TextBox();
+            wPos = new TextBox();
+            label10 = new Label();
+            zScale = new TextBox();
+            xPos = new TextBox();
+            yScale = new TextBox();
+            labelScale = new Label();
+            zRot = new TextBox();
+            label7 = new Label();
+            xScale = new TextBox();
+            labelPos = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            labelRot = new Label();
+            zPos = new TextBox();
+            yPos = new TextBox();
+            xRot = new TextBox();
+            yRot = new TextBox();
+            typeBox = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            typeNum = new NumericUpDown();
+            subtypeNum = new NumericUpDown();
+            subtypeBox = new ComboBox();
+            saveBtn = new Button();
+            ((System.ComponentModel.ISupportInitialize)typeNum).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)subtypeNum).BeginInit();
+            SuspendLayout();
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.ForeColor = SystemColors.Control;
+            label12.Location = new Point(12, 109);
+            label12.Name = "label12";
+            label12.Size = new Size(118, 20);
+            label12.TabIndex = 98;
+            label12.Text = "Geometry Name";
+            // 
+            // wScale
+            // 
+            wScale.Location = new Point(208, 275);
+            wScale.Name = "wScale";
+            wScale.Size = new Size(80, 27);
+            wScale.TabIndex = 94;
+            wScale.Text = "0";
+            // 
+            // modelName
+            // 
+            modelName.Location = new Point(142, 73);
+            modelName.MaxLength = 32;
+            modelName.Name = "modelName";
+            modelName.Size = new Size(146, 27);
+            modelName.TabIndex = 95;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.ForeColor = SystemColors.Control;
+            label11.Location = new Point(12, 76);
+            label11.Name = "label11";
+            label11.Size = new Size(96, 20);
+            label11.TabIndex = 97;
+            label11.Text = "Model Name";
+            // 
+            // wRot
+            // 
+            wRot.Location = new Point(122, 275);
+            wRot.Name = "wRot";
+            wRot.Size = new Size(80, 27);
+            wRot.TabIndex = 93;
+            wRot.Text = "0";
+            // 
+            // geomName
+            // 
+            geomName.Location = new Point(142, 106);
+            geomName.MaxLength = 32;
+            geomName.Name = "geomName";
+            geomName.Size = new Size(146, 27);
+            geomName.TabIndex = 96;
+            // 
+            // wPos
+            // 
+            wPos.Location = new Point(36, 275);
+            wPos.Name = "wPos";
+            wPos.Size = new Size(80, 27);
+            wPos.TabIndex = 92;
+            wPos.Text = "0";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.ForeColor = SystemColors.Control;
+            label10.Location = new Point(12, 278);
+            label10.Name = "label10";
+            label10.Size = new Size(23, 20);
+            label10.TabIndex = 91;
+            label10.Text = "W";
+            // 
+            // zScale
+            // 
+            zScale.Location = new Point(208, 242);
+            zScale.Name = "zScale";
+            zScale.Size = new Size(80, 27);
+            zScale.TabIndex = 84;
+            zScale.Text = "1";
+            // 
+            // xPos
+            // 
+            xPos.Location = new Point(36, 176);
+            xPos.Name = "xPos";
+            xPos.Size = new Size(80, 27);
+            xPos.TabIndex = 76;
+            xPos.Text = "0";
+            // 
+            // yScale
+            // 
+            yScale.Location = new Point(208, 209);
+            yScale.Name = "yScale";
+            yScale.Size = new Size(80, 27);
+            yScale.TabIndex = 83;
+            yScale.Text = "1";
+            // 
+            // labelScale
+            // 
+            labelScale.AutoSize = true;
+            labelScale.ForeColor = SystemColors.Control;
+            labelScale.Location = new Point(208, 152);
+            labelScale.Name = "labelScale";
+            labelScale.Size = new Size(44, 20);
+            labelScale.TabIndex = 90;
+            labelScale.Text = "Scale";
+            // 
+            // zRot
+            // 
+            zRot.Location = new Point(122, 242);
+            zRot.Name = "zRot";
+            zRot.Size = new Size(80, 27);
+            zRot.TabIndex = 81;
+            zRot.Text = "0";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.ForeColor = SystemColors.Control;
+            label7.Location = new Point(12, 212);
+            label7.Name = "label7";
+            label7.Size = new Size(17, 20);
+            label7.TabIndex = 86;
+            label7.Text = "Y";
+            // 
+            // xScale
+            // 
+            xScale.Location = new Point(208, 176);
+            xScale.Name = "xScale";
+            xScale.Size = new Size(80, 27);
+            xScale.TabIndex = 82;
+            xScale.Text = "1";
+            // 
+            // labelPos
+            // 
+            labelPos.AutoSize = true;
+            labelPos.ForeColor = SystemColors.Control;
+            labelPos.Location = new Point(36, 153);
+            labelPos.Name = "labelPos";
+            labelPos.Size = new Size(61, 20);
+            labelPos.TabIndex = 88;
+            labelPos.Text = "Position";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.ForeColor = SystemColors.Control;
+            label8.Location = new Point(12, 179);
+            label8.Name = "label8";
+            label8.Size = new Size(18, 20);
+            label8.TabIndex = 85;
+            label8.Text = "X";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.ForeColor = SystemColors.Control;
+            label9.Location = new Point(12, 245);
+            label9.Name = "label9";
+            label9.Size = new Size(18, 20);
+            label9.TabIndex = 87;
+            label9.Text = "Z";
+            // 
+            // labelRot
+            // 
+            labelRot.AutoSize = true;
+            labelRot.ForeColor = SystemColors.Control;
+            labelRot.Location = new Point(122, 152);
+            labelRot.Name = "labelRot";
+            labelRot.Size = new Size(66, 20);
+            labelRot.TabIndex = 89;
+            labelRot.Text = "Rotation";
+            // 
+            // zPos
+            // 
+            zPos.Location = new Point(36, 242);
+            zPos.Name = "zPos";
+            zPos.Size = new Size(80, 27);
+            zPos.TabIndex = 78;
+            zPos.Text = "0";
+            // 
+            // yPos
+            // 
+            yPos.Location = new Point(36, 209);
+            yPos.Name = "yPos";
+            yPos.Size = new Size(80, 27);
+            yPos.TabIndex = 77;
+            yPos.Text = "0";
+            // 
+            // xRot
+            // 
+            xRot.Location = new Point(122, 176);
+            xRot.Name = "xRot";
+            xRot.Size = new Size(80, 27);
+            xRot.TabIndex = 79;
+            xRot.Text = "-0";
+            // 
+            // yRot
+            // 
+            yRot.Location = new Point(122, 209);
+            yRot.Name = "yRot";
+            yRot.Size = new Size(80, 27);
+            yRot.TabIndex = 80;
+            yRot.Text = "-0";
+            // 
+            // typeBox
+            // 
+            typeBox.FormattingEnabled = true;
+            typeBox.Location = new Point(160, 6);
+            typeBox.Name = "typeBox";
+            typeBox.Size = new Size(128, 28);
+            typeBox.TabIndex = 100;
+            typeBox.SelectedIndexChanged += typeBox_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = SystemColors.Control;
+            label1.Location = new Point(12, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 20);
+            label1.TabIndex = 102;
+            label1.Text = "Sub-Type";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = SystemColors.Control;
+            label2.Location = new Point(12, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(40, 20);
+            label2.TabIndex = 101;
+            label2.Text = "Type";
+            // 
+            // typeNum
+            // 
+            typeNum.Location = new Point(98, 7);
+            typeNum.Maximum = new decimal(new int[] { 65535, 0, 0, 0 });
+            typeNum.Name = "typeNum";
+            typeNum.Size = new Size(56, 27);
+            typeNum.TabIndex = 103;
+            typeNum.ValueChanged += typeNum_ValueChanged;
+            // 
+            // subtypeNum
+            // 
+            subtypeNum.Location = new Point(98, 40);
+            subtypeNum.Maximum = new decimal(new int[] { 65535, 0, 0, 0 });
+            subtypeNum.Name = "subtypeNum";
+            subtypeNum.Size = new Size(56, 27);
+            subtypeNum.TabIndex = 104;
+            subtypeNum.ValueChanged += subtypeNum_ValueChanged;
+            // 
+            // subtypeBox
+            // 
+            subtypeBox.FormattingEnabled = true;
+            subtypeBox.Location = new Point(160, 39);
+            subtypeBox.Name = "subtypeBox";
+            subtypeBox.Size = new Size(128, 28);
+            subtypeBox.TabIndex = 105;
+            subtypeBox.SelectedIndexChanged += subtypeBox_SelectedIndexChanged;
+            // 
+            // saveBtn
+            // 
+            saveBtn.Location = new Point(36, 312);
+            saveBtn.Name = "saveBtn";
+            saveBtn.Size = new Size(252, 29);
+            saveBtn.TabIndex = 106;
+            saveBtn.Text = "Create New Node";
+            saveBtn.UseVisualStyleBackColor = true;
+            saveBtn.Click += saveBtn_Click;
+            // 
+            // AddNode
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(64, 64, 68);
+            ClientSize = new Size(307, 353);
+            Controls.Add(saveBtn);
+            Controls.Add(subtypeBox);
+            Controls.Add(subtypeNum);
+            Controls.Add(typeNum);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Controls.Add(typeBox);
+            Controls.Add(label12);
+            Controls.Add(wScale);
+            Controls.Add(modelName);
+            Controls.Add(label11);
+            Controls.Add(wRot);
+            Controls.Add(geomName);
+            Controls.Add(wPos);
+            Controls.Add(label10);
+            Controls.Add(zScale);
+            Controls.Add(xPos);
+            Controls.Add(yScale);
+            Controls.Add(labelScale);
+            Controls.Add(zRot);
+            Controls.Add(label7);
+            Controls.Add(xScale);
+            Controls.Add(labelPos);
+            Controls.Add(label8);
+            Controls.Add(label9);
+            Controls.Add(labelRot);
+            Controls.Add(zPos);
+            Controls.Add(yPos);
+            Controls.Add(xRot);
+            Controls.Add(yRot);
+            Name = "AddNode";
+            ShowIcon = false;
+            Text = "Create Node";
+            Load += AddNode_Load;
+            ((System.ComponentModel.ISupportInitialize)typeNum).EndInit();
+            ((System.ComponentModel.ISupportInitialize)subtypeNum).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label12;
+        private TextBox wScale;
+        private TextBox modelName;
+        private Label label11;
+        private TextBox wRot;
+        private TextBox geomName;
+        private TextBox wPos;
+        private Label label10;
+        private TextBox zScale;
+        private TextBox xPos;
+        private TextBox yScale;
+        private Label labelScale;
+        private TextBox zRot;
+        private Label label7;
+        private TextBox xScale;
+        private Label labelPos;
+        private Label label8;
+        private Label label9;
+        private Label labelRot;
+        private TextBox zPos;
+        private TextBox yPos;
+        private TextBox xRot;
+        private TextBox yRot;
+        private ComboBox typeBox;
+        private Label label1;
+        private Label label2;
+        private NumericUpDown typeNum;
+        private NumericUpDown subtypeNum;
+        private ComboBox subtypeBox;
+        private Button saveBtn;
+    }
+}
---
+++ ChainPellet/AddNode.cs
@@ -0,0 +1,128 @@
+﻿using System.Numerics;
+using static ChainPellet.sf;
+
+namespace ChainPellet
+{
+    public partial class AddNode : Form
+    {
+        public SceneNode loadedNode;
+        public bool loading;
+        public AddNode()
+        {
+            InitializeComponent();
+        }
+
+        private void AddNode_Load(object sender, EventArgs e)
+        {
+            string[] types = Enum.GetNames(typeof(SceneModelType));
+            string[] subtypes = Enum.GetNames(typeof(SceneGeomFormat));
+            typeBox.Items.AddRange(types);
+            subtypeBox.Items.AddRange(subtypes);
+            typeNum.Value = 0;
+            subtypeNum.Value = 0;
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SceneNode node = new SceneNode();
+                node.model_name = modelName.Text;
+                node.geom_name = geomName.Text;
+                float X = float.Parse(xPos.Text);
+                float Y = float.Parse(yPos.Text);
+                float Z = float.Parse(zPos.Text);
+                float W = float.Parse(wPos.Text);
+                node.pos = new Vector4(X, Y, Z, W);
+                X = float.Parse(xRot.Text);
+                Y = float.Parse(yRot.Text);
+                Z = float.Parse(zRot.Text);
+                W = float.Parse(wRot.Text);
+                node.rot = new Vector4(X, Y, Z, W);
+                X = float.Parse(xScale.Text);
+                Y = float.Parse(yScale.Text);
+                Z = float.Parse(zScale.Text);
+                W = float.Parse(wScale.Text);
+                node.scale = new Vector4(X, Y, Z, W);
+                uint newType = (uint)typeNum.Value;
+                uint subType = (uint)subtypeNum.Value;
+
+                NodeData nd_data = new NodeData();
+                nd_data.type = (SceneModelType)newType;
+                nd_data.subType = subType;
+                nd_data.light = new Light();
+                nd_data.array = new PointArray();
+                nd_data.camera = new Camera();
+                nd_data.bounds = new Bounds();
+                nd_data.bezier = new Bezier();
+                nd_data.unknown = new Unknown();
+                nd_data.unknown.data = new byte[0];
+
+                nd_data.array.points = new Vector4[0];
+                nd_data.bezier.control_points = new Vector4[0];
+                nd_data.bezier.knots = new float[0];
+                node.data = nd_data;
+                loadedNode = node;
+                DialogResult = DialogResult.OK;
+            }
+            catch
+            {
+                MessageBox.Show("Failed to parse information into a Node.");
+            }
+        }
+
+        private void typeNum_ValueChanged(object sender, EventArgs e)
+        {
+            if (loading) return;
+            loading = true;
+            try
+            {
+                typeBox.SelectedItem = ((SceneModelType)typeNum.Value).ToString();
+            }
+            catch
+            {
+                typeBox.SelectedIndex = -1;
+            }
+            loading = false;
+        }
+
+        private void subtypeNum_ValueChanged(object sender, EventArgs e)
+        {
+            if (loading) return;
+            loading = true;
+            try
+            {
+                subtypeBox.SelectedItem = ((SceneGeomFormat)subtypeNum.Value).ToString();
+            }
+            catch
+            {
+                subtypeBox.SelectedIndex = -1;
+            }
+            loading = false;
+        }
+
+        private void typeBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (loading) return;
+            loading = true;
+            try
+            {
+                typeNum.Value = (int)Enum.Parse(typeof(SceneModelType), typeBox.SelectedItem.ToString());
+            }
+            catch { }
+            loading = false;
+        }
+
+        private void subtypeBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (loading) return;
+            loading = true;
+            try
+            {
+                subtypeNum.Value = (int)Enum.Parse(typeof(SceneGeomFormat), subtypeBox.SelectedItem.ToString());
+            }
+            catch { }
+            loading = false;
+        }
+    }
+}
